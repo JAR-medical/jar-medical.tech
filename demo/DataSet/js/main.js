a5a6baf8a1da8fce1a79b70529032af4164f492e
Alexander Kluge <alexanderh2seo4@gmail.com>
2026-08-25 02:16:01 +0200

    Refresh PNG sticker asset cache

--- demo/DataSet/js/main.js
+++ demo/DataSet/js/main.js
@@ -11,7 +11,7 @@ import { GameAudio } from "./audio.js";
 import { HOTBAR_ITEMS } from "./items.js";
 import { randomSeed } from "./cases.js";
 import { Campaign } from "./campaign.js";
-import { IntroSequence } from "./intro.js?v=20260825-stickers1";
+import { IntroSequence } from "./intro.js?v=20260825-stickers2";
 import { LEVELS } from "./levels.js";
 import { ContributionClient } from "./contributions.js?v=20260825-role2";
 import {
